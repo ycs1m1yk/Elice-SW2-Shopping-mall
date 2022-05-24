c2c4d471dfb23c07a1c9a1c53884a11990dc07a0
Jaehyeok <milkboy2564@naver.com>
2022-05-25 00:37:23 +0900

    Security Page UI 완성

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -11,6 +11,7 @@ viewsRouter.use("/register", serveStatic("register"));
 viewsRouter.use("/login", serveStatic("login"));
 viewsRouter.use("/account", serveStatic("account"));
 viewsRouter.use("/account/orders", serveStatic("account/orders", "orders"));
+viewsRouter.use("/account/security", serveStatic("account/security", "security"));
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use("/", serveStatic(""));
