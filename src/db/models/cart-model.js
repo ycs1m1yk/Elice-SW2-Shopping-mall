9d49c9e8b5c3d354ee8ba53d1e527dd881eb8ba4
chss3339 <chss3339@gmail.com>
2022-05-24 15:49:42 +0900

    logout

---
+++ src/db/models/cart-model.js
@@ -0,0 +1,15 @@
+import { model } from 'mongoose';
+import { CartSchema } from '../schemas/Cart-schema';
+
+const Cart = model('Carts', CartSchema);
+
+export class CartModel {
+    async findByCustomerEmail(email) {
+      const cartLists = await Cart.find({email});
+      return cartLists
+    }
+  }
+  
+  const cartModel = new CartModel();
+  
+  export { cartModel };
