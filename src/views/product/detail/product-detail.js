10c2d43c6e2c8ad6d60846cf7f1bb8f7f8953e97
Jaehyeok <milkboy2564@naver.com>
2022-05-27 17:25:54 +0900

    Add: 각 상품에 맞는 정보 화면에 출력

--- src/views/product/detail/product-detail.js
+++ src/views/product/detail/product-detail.js
@@ -3,12 +3,26 @@ import header from "/components/Header.js";
 
 document.body.insertAdjacentElement("afterbegin", header);
 
+const paintProduct = (product) => {
+  const productImageTag = document.getElementById("productImageTag");
+  const manufacturerTag = document.getElementById("manufacturerTag");
+  const titleTag = document.getElementById("titleTag");
+  const priceTag = document.getElementById("priceTag");
+  const detailDescriptionTag = document.getElementById("detailDescriptionTag");
+
+  productImageTag.src = product.img;
+  manufacturerTag.innerText = product.brandName;
+  titleTag.innerText = product.name;
+  priceTag.innerText = product.price;
+  detailDescriptionTag.innerText = product.detailDescription;
+};
+
 const getDataFromApi = async () => {
   const url = new URL(location.href);
   const productId = url.searchParams.get("id");
 
   const product = await Api.get("/api/product", productId);
-  console.log(product);
+  paintProduct(product);
 };
 
 getDataFromApi();
