02a8afd09138f548c6c4ce47f57e1bbdd0998c51
TJ <xowns1016@gmail.com>
2022-06-02 00:58:45 +0900

    주석 오류 수정

--- src/routers/category-router.js
+++ src/routers/category-router.js
@@ -53,7 +53,7 @@ categoryRouter.get("/:id", loginRequired, async function (req, res, next) {
 
     const categoryId = req.params.id;
     const categoryInfo = await categoryService.getCategoryById(categoryId);
-    // 상품 스키마를 JSON 형태로 프론트에 보냄
+    // 카테고리 정보를 JSON 형태로 프론트에 보냄
     res.status(200).json(categoryInfo);
   } catch (error) {
     next(error);
