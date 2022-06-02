e8302b5956ddc16733a829d496a1bfd04e604efa
TJ <xowns1016@gmail.com>
2022-06-03 02:44:44 +0900

    brandName 스키마 수정

--- src/db/schemas/product-schema.js
+++ src/db/schemas/product-schema.js
@@ -25,8 +25,7 @@ const ProductSchema = new Schema(
     },
     brandName: {
       type: String,
-      required: false,
-      default: true,
+      required: true,
     },
     keyword: {
       type: [String],
