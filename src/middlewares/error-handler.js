91cf4cac37e0838caeecdd63542d9c5c681a6a93
TJ <xowns1016@gmail.com>
2022-06-03 01:42:36 +0900

    에러핸들러 수정

--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -3,9 +3,23 @@
 function errorHandler(error, req, res, next) {
   // 터미널에 노란색으로 출력됨.
   console.log("\x1b[33m%s\x1b[0m", error.stack);
+  
+  switch (error.message) {
+    case "Unauthorized":
+      res.status(401).json({ result: "error", reason: error.message });
+      break;
+    
+    case "Forbidden":
+      res.status(404).json({ result: "error", reason: error.message });
+      break;
+    // case "No Acceptable":
+    case "headers의 Content-Type을 application/json으로 설정해주세요":
+      res.status(406).json({ result: "error", reason: error.message });
+    
+    default: 
+      res.status(400).json({ result: "error", reason: error.message });
+  }
 
-  // 에러는 400 코드의 JSON 형태로 프론트에 전달됨
-  res.status(400).json({ result: "error", reason: error.message });
 }
 
 export { errorHandler };
