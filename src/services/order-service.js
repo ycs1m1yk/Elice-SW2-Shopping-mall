32f04c02d0fdcfc18bb398273bb5399566a87e5a
TJ <xowns1016@gmail.com>
2022-06-03 18:59:56 +0900

    orderSevice 주석 수정

--- src/services/order-service.js
+++ src/services/order-service.js
@@ -1,12 +1,22 @@
 import { orderModel } from "../db";
-
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-
 class OrderService {
-  // 본 파일의 맨 아래에서, new OrderService(orderModel) 하면, 이 함수의 인자로 전달됨
   constructor(orderModel) {
     this.orderModel = orderModel;
+  }
+  // 주문 목록 전체를 받음.
+  async getOrdersAll() {
+    return await this.orderModel.findByUserId();
+  }
+
+  
+  // 개인의 주문 목록 조회
+  async getOrders(userId) {
+    return await this.orderModel.findByUserId(userId);
+  }
+
+  // 오더 id로 주문 조회
+  async getOrdersByOrderId(orderId) {
+    return await this.orderModel.findById(orderId);
   }
 
   // 주문하기
@@ -24,39 +34,30 @@ class OrderService {
       shippingFee,
     };
 
-    // db에 저장
+    // db에 주문 정보 저장
     const createdNewOrder = await this.orderModel.create(newOrderInfo);
     const checkNewOrder = await this.orderModel.findById({
       _id: createdNewOrder._id,
     });
+    // db 데이터 확인
     if (createdNewOrder.id !== checkNewOrder.id) {
       throw new Error("주문이 정상적으로 완료되지 않았습니다.");
     }
     return createdNewOrder;
   }
 
-  // 개인의 주문 목록을 받음.
-  async getOrders(userId) {
-    return await this.orderModel.findByUserId(userId);
-  }
-
-  async getOrdersByOrderId(orderId) {
-    return await this.orderModel.findById(orderId);
-  }
-
+  // 주문 취소 list 얻기
   async getOrdersForDelete(orderIdList) {
     const orderList = [];
-    for await (const orderId of orderIdList) {
+    // db에서 삭제할 주문 정보 가져오기
+    for (const orderId of orderIdList) {
       const order = await this.orderModel.findById(orderId);
       orderList.push(order);
     }
     return orderList;
   }
 
-  // 주문 목록 전체를 받음.
-  async getOrdersAll() {
-    return await this.orderModel.findByUserId();
-  }
+
 
   // 주문 취소
   async deleteOrder(orderIdArray) {
@@ -66,11 +67,13 @@ class OrderService {
   }
 
   async deleteOrderProduct({ orderId, productId }) {
-    const deleteOrderList = await this.orderModel.findById(orderId);
-    if (!deleteOrderList) {
+    const deleteOrderInfo = await this.orderModel.findById(orderId);
+    // 주문 정보 데이터 확인
+    if (!deleteOrderInfo) {
       throw new Error("요청된 주문이 없습니다.");
     }
-    const newDelete = await deleteOrderList.orderList.filter(
+    // 주문 list 가져오기
+    const newDelete = await deleteOrderInfo.orderList.filter(
       (e) => e.productId !== productId
     );
     const deleteUpdate = { $set: { orderList: newDelete } };
@@ -78,6 +81,7 @@ class OrderService {
       orderId,
       update: deleteUpdate,
     });
+
     if (newOrder.orderList.length < 1) {
       return await this.orderModel.deleteById(orderId);
     }
