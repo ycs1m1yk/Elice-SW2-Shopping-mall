ddb5852997262c2e0c01085280fbeb3a347770a3
TJ <xowns1016@gmail.com>
2022-06-03 21:23:28 +0900

    orderService 수정

--- src/services/order-service.js
+++ src/services/order-service.js
@@ -24,7 +24,10 @@ class OrderService {
     // 객체 destructuring, address는 타입 객체, orderList는 타입 배열
     const { userId, address, request, orderList, totalPrice, shippingFee } =
       orderInfo;
-
+    // body 데이터 검증
+    if (!userId || !address || !orderList || !totalPrice) {
+      throw new Error("주문 정보를 올바르게 입력해주세요.")
+    }
     const newOrderInfo = {
       userId,
       address,
@@ -36,13 +39,6 @@ class OrderService {
 
     // db에 주문 정보 저장
     const createdNewOrder = await this.orderModel.create(newOrderInfo);
-    const checkNewOrder = await this.orderModel.findById({
-      _id: createdNewOrder._id,
-    });
-    // db 데이터 확인
-    if (createdNewOrder.id !== checkNewOrder.id) {
-      throw new Error("주문이 정상적으로 완료되지 않았습니다.");
-    }
     return createdNewOrder;
   }
 
@@ -57,8 +53,6 @@ class OrderService {
     return orderList;
   }
 
-
-
   // 주문 취소
   async deleteOrder(orderIdArray) {
     return await orderIdArray.map((orderId) =>
@@ -68,27 +62,29 @@ class OrderService {
 
   async deleteOrderProduct({ orderId, productId, userId }) {
     const orderInfo = await this.getOrdersByOrderId(orderId);
+    // 자신의 주문만 취소 가능
     if (userId !== orderInfo.userId) {
-      throw new Error("본인의 주문 내역만 취소할 수 있습니다.");
+      throw new Error("Forbbiden");
     }
-    const deleteOrderList = await this.orderModel.findById(orderId);
-    if (!deleteOrderList) {
+    // 주문 취소 정보 가져오기
+    const deleteOrderInfo = await this.orderModel.findById(orderId);
+    if (!deleteOrderInfo) {
       throw new Error("요청된 주문이 없습니다.");
     }
-    // 주문 list 가져오기
+    // 주문 개별 취소 작업
     const newDelete = await deleteOrderInfo.orderList.filter(
       (e) => e.productId !== productId
     );
     const deleteUpdate = { $set: { orderList: newDelete } };
+    // 취소된 주문을 반영하여 주문 정보 업데이트
     const newOrder = await this.orderModel.update({
       orderId,
       update: deleteUpdate,
     });
-
     if (!newOrder) {
       throw new Error("주문이 정상적으로 취소되지 않았습니다.");
     }
-
+    // 주문정보에 주문 상품이 없으면 주문 정보 삭제
     if (newOrder.orderList.length < 1) {
       return await this.orderModel.deleteById(orderId);
     }
